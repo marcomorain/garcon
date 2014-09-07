77990b34a9b1d6ecefa9d9afb7a5defd422f08c0
Marc O'Morain <github.com@marcomorain.com>
2014-09-07 15:41:33 +0100

    URLs

--- garcon.c
+++ garcon.c
@@ -9,29 +9,48 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <assert.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/uio.h>
+#include <fcntl.h>
+
 #include "http_parser.h"
+#include "buffer.h"
 
-
-char url_buffer[1024];
+struct parser_data {
+  buffer_t* url;
+};
 
 static int on_url(http_parser* parser, const char *at, size_t len)
 {
-  assert(len < 1024);
-  memcpy(url_buffer, at, len);
-  url_buffer[len] = 0;
-  (void)parser;
-  printf("URL>>>> '%s'\n", url_buffer);
+  struct parser_data* data = parser->data;
+  buffer_append_n(data->url, at, len);
+  printf("Append %zu bytes to URL\n", len);
   return 0;
 }
 
-static void send_file(int socket, char* filename) {
-  FILE* file = fopen(filename, "rb");
-  if (!file) {
-    fprintf(stderr, "Cannot open %s", filename);
+static void send_file(int socket, const char* root, const char* filename) {
+
+
+  buffer_t* buffer = buffer_new();
+  buffer_append(buffer, root);
+  buffer_append(buffer, filename);
+
+  int file = open(buffer->data, O_RDONLY);
+  if (file <= 0) {
+    fprintf(stderr, "Cannot open %s\n", filename);
     return;
   }
-// READ FILE
-  fclose(file);
+
+  // Whole file
+  off_t length = 0; 
+  int result = sendfile(file, socket, 0, &length, 0, 0);
+
+  if (result == -1) {
+    return;
+  }
+  // READ FILE
+  close(file);
 }
 
 int main(int argc, char** argv) {
@@ -41,20 +60,34 @@ int main(int argc, char** argv) {
   (void)argv;
   puts("Garçon!");
 
-   int create_socket, new_socket;
+  const uint16_t port = 8888;
+
+
+  const char* root = getcwd(0, 0);
+  printf("Serving content from %s\n", root);
+
+   
    socklen_t addrlen;
    struct sockaddr_in address;
 
+    int create_socket;
    if ((create_socket = socket(AF_INET, SOCK_STREAM, 0)) > 0){
       printf("The socket was created\n");
    }
 
    address.sin_family = AF_INET;
    address.sin_addr.s_addr = INADDR_ANY;
-   address.sin_port = htons(15000);
+   address.sin_port = htons(port);
 
+  int yes = 1;
+  if ( setsockopt(create_socket, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1 ) {
+    perror("setsockopt");
+  } 
+    
    if (bind(create_socket, (struct sockaddr *) &address, sizeof(address)) == 0){
-      printf("Binding Socket\n");
+      printf("Binding socket on port %d\n", port);
+   } else {
+    perror("bind failed");
    }
 
    http_parser_settings settings;
@@ -67,10 +100,14 @@ int main(int argc, char** argv) {
          exit(1);
       }
 
+      int new_socket;
+
       if ((new_socket = accept(create_socket, (struct sockaddr *) &address, &addrlen)) < 0) {
          perror("server: accept");
          exit(1);
       }
+
+
 
       if (new_socket > 0){
          printf("The Client is connected...\n");
@@ -85,18 +122,17 @@ int main(int argc, char** argv) {
         /* Handle error. */
       }
 
+      struct parser_data data;
+      data.url = buffer_new();
+
       http_parser *parser = malloc(sizeof(http_parser));
       http_parser_init(parser, HTTP_REQUEST);
-      parser->data = new_socket;
+      parser->data = &data;
 
       /* Start up / continue the parser.
        * Note we pass recved==0 to signal that EOF has been received.
        */
       size_t nparsed = http_parser_execute(parser, &settings, buf, recved);
-
-      printf("Parsed %x\n", nparsed);
-
-      free(parser);
 
       if (parser->upgrade) {
         /* handle new protocol */
@@ -104,7 +140,11 @@ int main(int argc, char** argv) {
         /* Handle error. Usually just close the connection. */
       }
 
-      write(new_socket, "hello world\n", 12);
+      const char* path = data.url->data;
+      send_file(new_socket, root, path);
+
+      free(parser);
+      buffer_free(data.url);
       close(new_socket);
    }
    close(create_socket);
